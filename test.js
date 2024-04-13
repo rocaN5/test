94fd9b5bacdc214bdb351eb5014fa9f0431026f1
iRock <47889456+rocaN5@users.noreply.github.com>
2024-04-13 07:40:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("Работает!!!!")
+$.getScript('https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js', function() {
+    var downloadButton = $("<button>").attr("id", "downloadButton").text("Download Links as PDF").appendTo("body");
+
+    $(document).on("click", "#downloadButton", function() {
+        var links = [];
+        $("a").each(function() {
+            links.push($(this).attr("href"));
+        });
+        var linksText = links.join("\n");
+        var doc = new jsPDF();
+        doc.text(linksText, 10, 10);
+        doc.save("links.pdf");
+    });
+});
